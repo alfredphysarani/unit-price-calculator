d5bbd2cd3a7dd7d30767b937582dfb2a0ea05a49
Alfred Cheung <36896899+alfredphysarani@users.noreply.github.com>
2023-08-29 22:50:18 +0800

    unit calculator
    MVP of the unit calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+const unitCalculator = (price, qty) => qty === 0 ? 'Qunatity cannot be zero!' : Math.round(price/qty*100)/100;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const calcBtn = document.getElementById('calculate-button');
+
+    calcBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const inputPrice = document.getElementById('price').value;
+        const inputQty = document.getElementById('qty').value;
+        const inputUnit = document.getElementById('units').value;
+
+        const result = unitCalculator(inputPrice, inputQty);
+        result === 'Qunatity cannot be zero!' ? document.getElementById('result-container').innerHTML = result :
+        document.getElementById('result-container').innerHTML = `The unit price: ${result} $/${inputUnit}`;
+    })
+})
